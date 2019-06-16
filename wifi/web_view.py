98c8d0820f3fdc7c4a0d1a79a636de804ad79d7f
Gady Rozin <gady.rozin@vonage.com>
2019-06-16 17:39:07 +0300

    Add Submit

--- wifi/web_view.py
+++ wifi/web_view.py
@@ -3,7 +3,7 @@ import os
 import os.path
 import time
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 
 import webview
 import sys
@@ -31,10 +31,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 # =========================================================
 
+
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 
 # =========================================================
+
+
 def get_file(filename):  # pragma: no cover
     try:
         src = os.path.join(root_dir(), filename)
@@ -48,40 +51,33 @@ def get_file(filename):  # pragma: no cover
         return str(exc)
 
 # =========================================================
+
+
 @app.route('/', methods=['GET'])
 def metrics():  # pragma: no cover
     content = get_file('index.html')
     return Response(content, mimetype="text/html")
 
-# =========================================================
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
-    #return render_template('index.html',**locals())
 
+# =========================================================
+
+
+@app.route('/run',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      #return render_template("result.html",result = result)
+      return metrics()
 # =========================================================
 
 
 def start_server():
-   #app.run()
    app.run(host='0.0.0.0', port=5000)
 
 # =========================================================
 
 
 if __name__ == '__main__':
-    """  https://github.com/r0x0r/pywebview/blob/master/examples/http_server.py
-    """
 
     t = threading.Thread(target=start_server)
     t.daemon = True
