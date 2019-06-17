d25177b7821e6825b34694f74e03c4783e568219
Gady Rozin <gady.rozin@vonage.com>
2019-06-17 12:00:13 +0300

    Add threading

--- wifi/web_view.py
+++ wifi/web_view.py
@@ -13,32 +13,55 @@ import threading
 # =========================================================
 
 
-def do_connect():
-    os.system("networksetup -setairportpower airport on")
-    print("Connected : %s" % time.ctime())
+class MyThread(threading.Thread):
+
+    def __init__(self, event):
+        threading.Thread.__init__(self)
+        self.stopped = event
+        self.disconnect_interval = 0
+        self.offline_time = 0
+
+    def set(self, _disconnect_interval , _offline_time ):
+        self.disconnect_interval = int(_disconnect_interval) * 1000
+        self.offline_time = int(_offline_time) * 1000
+
+    def run(self):
+        while not self.stopped.wait(self.disconnect_interval):
+            print("my thread")
+            self.do_disconnect()
+            time.sleep(self.offline_time)
+            self.do_connect()
+
+    # =========================================================
+
+
+    def do_connect(self):
+        os.system("networksetup -setairportpower airport on")
+        print("Connected : %s" % time.ctime())
+
+    # =========================================================
+
+    def do_disconnect(self):
+        os.system("networksetup -setairportpower airport off")
+        print("Disconnected : %s" % time.ctime())
 
 
 # =========================================================
 
-
-def do_disconnect():
-    os.system("networksetup -setairportpower airport off")
-    print("Disconnected : %s" % time.ctime())
-
-
-# =========================================================
 app = Flask(__name__)
 app.config.from_object(__name__)
+stopFlag = threading.Event()
+myThread = MyThread(stopFlag)
+
+
 # =========================================================
-
-
-def root_dir():  # pragma: no cover
+def root_dir():
     return os.path.abspath(os.path.dirname(__file__))
 
 # =========================================================
 
 
-def get_file(filename):  # pragma: no cover
+def get_file(filename):
     try:
         src = os.path.join(root_dir(), filename)
         # Figure out how flask returns static files
@@ -54,29 +77,41 @@ def get_file(filename):  # pragma: no cover
 
 
 @app.route('/', methods=['GET'])
-def metrics():  # pragma: no cover
-    content = get_file('index.html')
+def metrics(file_name = 'index.html' ):  # pragma: no cover
+    content = get_file(file_name)
     return Response(content, mimetype="text/html")
 
+# =========================================================
+
+
+@app.route('/stop',methods = ['POST', 'GET'])
+def stop():
+    global stopFlag
+    if request.method == 'POST':
+        stopFlag.set()
+        return metrics('index.html')
 
 # =========================================================
 
 
 @app.route('/run',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      #return render_template("result.html",result = result)
-      return metrics()
+def run():
+    global myThread
+    if request.method == 'POST':
+        result = request.form
+        disconnect_interval = result["disconnect"]
+        offline_time = result["offline"]
+        myThread.set(disconnect_interval,offline_time)
+        myThread.start()
+        return metrics('run.html')
 # =========================================================
 
 
 def start_server():
    app.run(host='0.0.0.0', port=5000)
 
+
 # =========================================================
-
-
 if __name__ == '__main__':
 
     t = threading.Thread(target=start_server)
